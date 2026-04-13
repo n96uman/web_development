d2d5dbb9cf461d32ef2e342691b64de9168f84cf
n96uman <numanfadil8@gmail.com>
2026-04-14 01:56:56 +0300

    server

--- Nodejs/server.js
+++ Nodejs/server.js
@@ -1,6 +1,9 @@
 const http = require('http');
 const server = http.createServer((req,res)=>{
-    console.log('request made to server')
+    console.log(req.url,req.method)
+    res.setHeader('Content-Type','text/html')
+    res.write('<h1>hello world</h1>')
+    res.end()
 });
 
 server.listen(3000,'localhost',()=>{
